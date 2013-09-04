36518a20bd96e1e31615625432d8e873a40ca6ed
Adam Jorgensen <adamj@sjsoft.com>
2013-09-04 11:59:31 +0200

    Added unique attribute to FieldDefinition class. Generate Data Template using Data Model.

--- extraction/api/datamodel.py
+++ extraction/api/datamodel.py
@@ -5,7 +5,7 @@ __author__ = 'aj@springlab.co'
 
 class FieldDefinition(object):
 
-    def __init__(self, type=str, default_value=None, required=False):
+    def __init__(self, type=str, default_value=None, required=False, unique=False):
         self.type = type
         self.default_value = default_value
         self.required = required
@@ -18,6 +18,7 @@ class DataModelPluginInterface(IPlugin.IPlugin):
 
     def get_data_model(self):
         return {
+            'key_field': FieldDefinition(unique=True),
             'int_field': FieldDefinition(int, required=False),
             'nested_object': {
                 'str_field': FieldDefinition(default_value='Yes')
@@ -31,19 +32,23 @@ class DataModelPluginInterface(IPlugin.IPlugin):
         }
 
     def get_data_template(self):
-        #TODO: Generate Template using Model?
-        return {
-            'int_field': 0,
-            'nested_object': {
-                'str_field': 'Hello World'
-            },
-            'nested_array': [
-                'Value'
-            ],
-            'nested_array_of_objects': [{
-                'int_field': 0,
-                'str_field': ''
-            }]
-        }
+        """ Generate a template dictionary object """
+        def generate_data_template(data_model):
+            template = {}
+            for field, field_definition in data_model.items():
+                field_value = None
+                if isinstance(field_definition, FieldDefinition):
+                    try:
+                        field_value = field_definition.default_value
+                        if not isinstance(field_value, field_definition.type):
+                            field_value = field_definition.default_value() if callable(field_definition.default_value) else field_definition.type()
+                    except:
+                        pass
+                elif isinstance(field_definition, (list, dict)):
+                    field_definition = field_definition[0] if isinstance(field_definition, list) else field_definition
+                    field_value = generate_data_template(field_definition)
+                template[field] = field_value
+            return template
+        return generate_data_template(self.get_data_model())
 
 
