f466a66d0a8424db2fc52d722f61037340763ccd
Adam Jorgensen <adamj@sjsoft.com>
2013-09-04 12:33:49 +0200

    Implemented SQLAlchemy Init

--- extraction/plugins/storage/SQLAlchemyFlatStorage.py
+++ extraction/plugins/storage/SQLAlchemyFlatStorage.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+from datetime import datetime
 from extraction.api import StoragePluginInterface
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, DateTime, BLOB
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, DateTime, BLOB, create_engine
 import json
 
 __author__ = 'aj@spinglab.co'
@@ -14,22 +17,36 @@ class StorageRecord(RecordBase):
     datamodel = Column(String, primary_key=True)
     uid = Column(String, primary_key=True)
     version = Column(Integer, primary_key=True)
-    created = Column(DateTime)
+    created = Column(DateTime, default=datetime.now)
     data = Column(BLOB)
 
 
-class SQLAlchemyFlatStoragePlugin(StoragePluginInterface):
+class SQLAlchemyFlatJSONStoragePlugin(StoragePluginInterface):
 
     def __init__(self, rdbms_url='sqlite:///SQLAlchemyFlatStoragePlugin.db'):
-        #TODO: Init SQLAlchemy
-        #TODO: Bind StorageRecord to DB
-        #TODO: Create DB Tables if necessary
-        pass
+        # Init SQLAlchemy
+        self.engine = create_engine(rdbms_url)
+        self.session_class = sessionmaker(self.engine)
+        RecordBase.metadata.create_all(self.engine)
+        # Define ContextManager
+        @contextmanager
+        def session_scope():
+            """Provide a transactional scope around a series of operations."""
+            session = self.session_class()
+            try:
+                yield session
+                session.commit()
+            except:
+                session.rollback()
+                raise
+            finally:
+                session.close()
+        self.session_scope = session_scope
 
     def can_store(self, data_model_name, data_model):
         return True
 
     def store(self, data_items, data_model_name, data_model):
-        return super(SQLAlchemyFlatStoragePlugin, self).store(data_items, data_model_name, data_model)
+        return super(SQLAlchemyFlatJSONStoragePlugin, self).store(data_items, data_model_name, data_model)
 
 
