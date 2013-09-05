353c18b8133abcbf766a9362754eec6c5f9b2506
Adam Jorgensen <adamj@sjsoft.com>
2013-09-05 10:25:22 +0200

    Caused PostProcessedDataItem to extend ExtractedDataItem. Updated signature of PostProcessingPluginInterface.process method. Documented PostProcessDataItem and PostProcessingPluginInterface classes.

--- extraction/api/postprocessing.py
+++ extraction/api/postprocessing.py
@@ -1,18 +1,35 @@
 from yapsy import IPlugin
+from extraction import ExtractedDataItem
 
 __author__ = 'aj@springlab.co'
 
 
-class PostProcessedDataItem(dict):
+class PostProcessedDataItem(ExtractedDataItem):
+    """
+    Overrides the ExtractedDataItem class to provide an indication that an
+    ExtractedDataItem instance has undergone post-processing.
+    """
     def __init__(self, seq=None, **kwargs):
         super(PostProcessedDataItem, self).__init__(seq, **kwargs)
 
 
 class PostProcessingPluginInterface(IPlugin.IPlugin):
+    """
+    Defines an interface for a plugin that processes data extracted from a source and transforms it in some fashion.
+    """
 
     def can_process(self, data_model_name, data_model):
+        """
+        Determines whether the plugin can process data associated with a given data model. Returns a bool.
+        """
         return False
 
-    def process(self, data_item, data_model_name, data_model):
-        return PostProcessedDataItem(data_item)
+    def process(self, data_items, data_model_name, data_model):
+        """
+        For a given data model, processes a dict mapping UID value -> ExtractedDataItem instance and transforms each
+        ExtractedDataItem instance into a PostProcessedDataItem instance.
+
+        Returns a dict mapping UID value -> PostProcessedDataItem instance.
+        """
+        return {}
 
