4d5de3db39539ddbfc6de1c207378eba9d796098
Adam Jorgensen <adamj@sjsoft.com>
2013-09-05 10:33:57 +0200

    Updated signature of ValidationPluginInterface.validate method. Documented ValidatedDataItem and ValidationPluginInterface classes.

--- extraction/api/validation.py
+++ extraction/api/validation.py
@@ -5,15 +5,31 @@ __author__ = 'aj@springlab.co'
 
 
 class ValidatedDataItem(PostProcessedDataItem):
+    """
+    Overrides the PostProcessedDataItem class to provide an indication that a
+    PostProcessedDataItem instance has undergone validation
+    """
     def __init__(self, seq=None, **kwargs):
         super(ValidatedDataItem, self).__init__(seq, **kwargs)
         self.valid = False
 
 
 class ValidationPluginInterface(IPlugin.IPlugin):
+    """
+    Defines an interface for a plugin that validates the processed data items
+    """
 
     def can_validate(self, data_model_name, data_model):
+        """
+        Determines whether the plugin can validate data associated with a given data model. Returns a bool.
+        """
         return False
 
-    def validate(self, data_item, data_model_name, data_model):
-        return ValidatedDataItem(data_item)
+    def validate(self, data_items, data_model_name, data_model):
+        """
+        For a given data model, processes a dict mapping UID value -> ExtractedDataItem instance and validates
+        each ExtractedDataItem instance, transforming it into a ValidatedDataItem instance in the process.
+
+        Returns a dict mapping UID value -> ValidatedDataItem instance
+        """
+        return {}
