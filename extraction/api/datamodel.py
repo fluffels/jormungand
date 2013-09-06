2e37e30f964e7fb47932239faf585f2b7391c5a0
Adam Jorgensen <adamj@sjsoft.com>
2013-09-06 10:32:01 +0200

    Removed support for callable default values. Fixed handling of lists of FieldDefinitions.

--- extraction/api/datamodel.py
+++ extraction/api/datamodel.py
@@ -63,21 +63,23 @@ class DataModelPluginInterface(IPlugin.IPlugin):
         This template dict instance can be copied using copy.deepcopy to provide a dict instance that merely needs
         to be "filled in" with values in order to produce a valid instance of the Data Model.
         """
+        def generate_field_value(field_definition):
+            try:
+                if isinstance(field_definition.default_value, field_definition.type):
+                    return field_definition.default_value
+                return field_definition.type()
+            finally:
+                return None
+
         def generate_data_template(data_model):
             template = {}
             for field, field_definition in data_model.items():
-                field_value = None
                 if isinstance(field_definition, FieldDefinition):
-                    try:
-                        field_value = field_definition.default_value
-                        if not isinstance(field_value, field_definition.type):
-                            field_value = field_definition.default_value() if callable(field_definition.default_value) else field_definition.type()
-                    except:
-                        pass
-                elif isinstance(field_definition, (list, dict)):
-                    field_definition = field_definition[0] if isinstance(field_definition, list) else field_definition
-                    field_value = generate_data_template(field_definition)
-                template[field] = field_value
+                    template[field] = generate_field_value(field_definition)
+                elif isinstance(field_definition, list):
+                    template[field] = [generate_field_value(field_definition) for field_definition in field_definition]
+                elif isinstance(field_definition, dict):
+                    template[field] = generate_data_template(field_definition)
             return template
         return generate_data_template(self.get_data_model())
 
