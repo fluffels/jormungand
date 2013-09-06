f0207a6d798d7e2df9aa7e868531f394e5e0932f
Adam Jorgensen <adamj@sjsoft.com>
2013-09-06 10:54:28 +0200

    Documented StoragePluginInterface class.

--- extraction/api/storage.py
+++ extraction/api/storage.py
@@ -4,9 +4,21 @@ __author__ = 'aj@springlab.co'
 
 
 class StoragePluginInterface(IPlugin.IPlugin):
+    """
+    Defines an interface for a plugin that stores data extracted from a source
+    """
 
     def can_store(self, data_model_name, data_model):
+        """
+        Determines whether the plugin can store data associated with a given data model. Returns a bool.
+        """
         return False
 
     def store(self, data_items, data_model_name, data_model):
+        """
+        For a given data model, receives an input list of (UID value, ExtractedDataItem instance) tuples and
+        attempts to store them.
+
+        Returns a boolean value indicating success or failure.
+        """
         return False
