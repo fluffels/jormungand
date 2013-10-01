e06fee48462ad17491b2ba44965c921850678f03
Adam Jorgensen <adamj@sjsoft.com>
2013-10-01 21:34:53 +0200

    Support more generic inputs by accepting a list of sources and processing them using urlparse.urlparse. urlparse.ParseResult instances are passed in to the can_extract and extract methods of ExtractionInterface plugin instances. In the case where the scheme of a ParseResult is either file or empty then the path represented is checked to determine if it is a directory. If so, the files within it are treated as individual sources.

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from copy import deepcopy
+from urlparse import urlparse
 import sys
 from extraction.api import *
 from extraction import ExtractionPluginManager
@@ -9,7 +10,7 @@ import logging
 __author__ = 'aj@springlab.co'
 
 
-def extract(json_config_file=None, plugin_roots=[], input_files=[], input_directories=[], logging=logging):
+def extract(json_config_file=None, plugin_roots=[], sources=[], logging=logging):
     """ Entry-point into the Extraction Process """
     #TODO: Refactor into separate functions or, possibly, a class in the extraction module
     # Init Plugin Manager and Plugins
@@ -29,13 +30,20 @@ def extract(json_config_file=None, plugin_roots=[], input_files=[], input_direct
         data_models[model_name] = plugin_info.plugin_object.get_data_model()
         data_templates[model_name] = plugin_info.plugin_object.get_data_template()
         logging.info('Loaded Data Model %s' % model_name)
-    # Obtain a full list of input files
-    logging.info('Generating list of files to process')
-    for input_directory in input_directories:
-        for directory, subdirectories, files in os.walk(input_directory):
-            input_files.extend([os.path.join(directory, file) for file in files])
-    input_files = [os.path.abspath(input_file) for input_file in input_files]
-    logging.info('Finalized list of files to process: %s' % ', '.join(input_files))
+    # Obtain a full list of sources
+    logging.info('Generating list of sources to process')
+    inputs = []
+    for source in sources:
+        source = urlparse(source)
+        if source.scheme in ('', 'file'):
+            if os.path.isdir(source.path):
+                for directory, subdirectories, files in os.walk(source.path):
+                    inputs.extend([urlparse(os.path.join(directory, file)) for file in files])
+            elif os.path.isfile(source.path):
+                inputs.append(source)
+        else:
+            inputs.append(source)
+    logging.info('Finalized list of files to process: %s' % ', '.join(inputs))
     #TODO: A fair bit of repeated code here, refactor
     # Extract data from input files
     logging.info('Extracting data')
@@ -43,11 +51,11 @@ def extract(json_config_file=None, plugin_roots=[], input_files=[], input_direct
     for plugin_info in plugin_manager.getPluginsOfCategory('Extraction'):
         plugin = plugin_info.plugin_object
         for data_model_name, data_model in data_models.items():
-            for input_file in input_files:
-                if plugin.can_extract(input_file, data_model_name, data_model):
+            for input in inputs:
+                if plugin.can_extract(input, data_model_name, data_model):
                     logging.info('Extracting data from %s using Data Model %s and Extraction plugin %s ' % (input_file, data_model_name, plugin))
                     extracted_data[data_model_name].extend(
-                        plugin.extract(input_file, data_model_name, data_model, deepcopy(data_templates[data_model_name])))
+                        plugin.extract(input, data_model_name, data_model, deepcopy(data_templates[data_model_name])))
     # Post-Process
     logging.info('Post-Processing Extracted Data')
     for plugin_info in plugin_manager.getPluginsOfCategory('PostProcessing'):
@@ -78,16 +86,15 @@ if __name__ == '__main__':
     # Ensure directory of extraction script is on the path
     sys.path.append(os.path.dirname(__file__))
     # Configure CLI parser
-    parser = ArgumentParser(description='Extract data from input files into a datastore')
+    parser = ArgumentParser(description='Extract data from input sources into a datastore, optionally performing post-processing and validation.')
     parser.add_argument('-c', '--config', default=None)
     parser.add_argument('-p', '--pluginroots', dest='plugin_roots', default=[], nargs='*', )
-    parser.add_argument('-f', '--inputfiles', dest='input_files', default=[], nargs='*')
-    parser.add_argument('-d', '--inputdirectories', dest='input_directories', default=[], nargs='*')
+    parser.add_argument('-s', '--source', dest='sources', default=[], nargs='*')
     parser.add_argument('--loglevel', default='INFO')
     args = parser.parse_args()
     # Configure Logging
     logging.basicConfig()
     logging = logging.getLogger('EXTRACTION')
     logging.setLevel(args.loglevel)
-    extract(args.config, args.plugin_roots, args.input_files, args.input_directories, logging)
+    extract(args.config, args.plugin_roots, args.sources, logging)
 
